3cca9d17c209e5e61a3db2d5ccf37d699be25901
David Cofer <dcofer@NeuroRoboticTech.com>
2012-11-25 13:59:05 +0000

    I got a basic simulation loop working with opencl. I can run a kernel, update Vm outputs, copy those onto the vm inputs, and then re-run the loop. I will next try and get a simple IGF model working.

--- trunk/Libraries/OpenNeuronCL/TestOpenCL.h
+++ trunk/Libraries/OpenNeuronCL/TestOpenCL.h
@@ -5,6 +5,10 @@ namespace OpenNeuronCL
 
 class OPENNEURONCL_PORT TestOpenCL
 {
+protected:
+	void RunFastSpikingNeurons();
+	void RunIDCheck();
+
 public:
 	TestOpenCL(void);
 	virtual ~TestOpenCL(void);
